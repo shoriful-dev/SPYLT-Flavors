e36cef6ce0e02ed4baaee16b84bba26b3a4b5050
shoriful-dev <cmshohelrana50@gmail.com>
2026-04-10 14:53:41 +0600

    create MessageSection

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import HeroSection from './sections/HeroSection';
 import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
 import gsap from 'gsap';
 import MessageSection from './sections/MessageSection';
-import FlavorSection from './sections/FlavorSection';
+// import FlavorSection from './sections/FlavorSection';
 import { useGSAP } from '@gsap/react';
-import NutritionSection from './sections/NutritionSection';
-import BenefitSection from './sections/BenefitSection';
-import TestimonialSection from './sections/TestimonialSection';
-import FooterSection from './sections/FooterSection';
+// import NutritionSection from './sections/NutritionSection';
+// import BenefitSection from './sections/BenefitSection';
+// import TestimonialSection from './sections/TestimonialSection';
+// import FooterSection from './sections/FooterSection';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -27,15 +27,15 @@ const App = () => {
         <div id="smooth-content">
           <HeroSection />
           <MessageSection />
-          <FlavorSection />
-          <NutritionSection />
+          {/* <FlavorSection /> */}
+          {/* <NutritionSection /> */}
 
           <div>
-            <BenefitSection />
-            <TestimonialSection />
+            {/* <BenefitSection /> */}
+            {/* <TestimonialSection /> */}
           </div>
 
-          <FooterSection />
+          {/* <FooterSection /> */}
         </div>
       </div>
     </main>
